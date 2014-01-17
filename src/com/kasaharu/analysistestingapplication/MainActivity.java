b83b6258ee8b9905cfe239bdd93edf4132247646
Wataru.Kasahara <kasahara1019@gmail.com>
2014-01-17 22:29:24 +0900

    [mod] Added the function of input password.

--- src/com/kasaharu/analysistestingapplication/MainActivity.java
+++ src/com/kasaharu/analysistestingapplication/MainActivity.java
@@ -3,13 +3,20 @@ package com.kasaharu.analysistestingapplication;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	Button mNextButton;
+	EditText mInputPassWord;
+
+	private final String CORRECTPASS = "test";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,6 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -28,12 +34,31 @@ public class MainActivity extends Activity {
 	public void onStart() {
 		super.onStart();
 		mNextButton = (Button)findViewById(R.id.btnNext);
+		mInputPassWord = (EditText)findViewById(R.id.editTxtPW);
 		mNextButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, NextActivity.class);
-				startActivity(intent);
+				boolean isCorrectPW = false;
+				isCorrectPW = IsCorrectPassWord();
+				if (isCorrectPW) {
+    				Intent intent = new Intent(MainActivity.this, NextActivity.class);
+    				startActivity(intent);
+				} else {
+			        Toast toast = Toast.makeText(MainActivity.this, "パスワードが間違っています。", Toast.LENGTH_SHORT);
+			        toast.setGravity(Gravity.CENTER, 0, 0);
+			        toast.show();
+				}
 			}
 		});
+	}
+
+	private boolean IsCorrectPassWord() {
+		String inputText = mInputPassWord.getText().toString();
+		Log.d("forDebug", "inputText = " + inputText);
+		if (inputText.equals(CORRECTPASS)) {
+			return true;
+		} else {
+    		return false;
+		}
 	}
 }
